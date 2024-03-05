498d4ebd5cb65d9fdf4fe5b8225c9eb53e1ad8fa
myweb1 <119072006+myweb1@users.noreply.github.com>
2024-03-04 21:03:29 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var impuestos = 10
+var puntos  = 10
+var windowreference;
+function dinerogenera() {
+    puntos += 1
+    document.getElementById("lolipo").innerHTML = puntos
+    console.log(puntos)
+    impuestoss()
+}
+function impuestoss() {
+    impuestos -= 1
+    document.getElementById("impuestos").innerHTML = impuestos
+    if (impuestos <= 0) {
+        puntos -= 10
+        impuestos =  Math.floor(Math.random() * 30 );
+    }
+    if (puntos <= 10) {
+        document.getElementById("perd").innerHTML = "perdisteeee!! bancarota"
+
+    }
+}
